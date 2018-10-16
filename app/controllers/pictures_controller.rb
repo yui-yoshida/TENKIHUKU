7ad0122b99d196abab2c46d050b8bf22120135cb
github yui-yoshida <yui.0618.kj8@gmail.com>
2018-10-16 13:58:11 +0900

    TENKIHUKU投稿前に確認画面を挟む機能を追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,8 +1,12 @@
 class PicturesController < ApplicationController
-before_action :set_picture, only: [:show, :edit, :update]
+before_action :set_picture, only: [:show, :edit, :update, :destroy]
 
   def new
-    @picture = Picture.new
+    if params[:back]
+      @picture = Picture.new(picture_params)
+    else
+      @picture = Picture.new
+    end
   end
 
   def create
@@ -26,13 +30,24 @@ before_action :set_picture, only: [:show, :edit, :update]
   end
 
   def update
-    
+
     if @picture.update(picture_params)
-      redirect_to picture_path(@picture.id)
+      redirect_to picture_path(@picture.id), notice:"TENKIHUKUを編集しました"
     else
       render "edit"
     end
   end
+
+  def destroy
+    @picture.destroy
+    redirect_to pictures_path, notice:"TENKIHUKUを削除しました"
+  end
+
+  def confirm
+    @picture = Picture.new(picture_params)
+    render :new if @picture.invalid?
+  end
+
   private
 
   def picture_params
