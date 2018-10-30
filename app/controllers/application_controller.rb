7441ceef7e3f63c03acb1d746cd62eb3b6463a12
github yui-yoshida <yui.0618.kj8@gmail.com>
2018-10-30 17:12:18 +0900

    darkskyAPIを取得

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
-before_action :configure_permitted_parameters, if: :devise_controller?
-before_action :sign_in_required, only: [:mypage]
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :sign_in_required, only: [:mypage]
   def after_sign_in_path_for(resource)
     mypage_user_path(current_user)
   end
@@ -10,14 +11,16 @@ before_action :sign_in_required, only: [:mypage]
   end
 
   private
+
     def sign_in_required
       redirect_to new_user_session_url unless user_signed_in?
     end
 
   protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :icon])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :icon])
-  end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :icon])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :icon])
+    end
 
 end
