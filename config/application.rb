520683eb4b4ff41b80f7ecd1b61792fc369a2756
github yui-yoshida <yui.0618.kj8@gmail.com>
2018-11-11 23:02:31 +0900

    config/application.rbにconfig.assets.initialize_on_precompile = falseを追加

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module TENKIHUKU
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
