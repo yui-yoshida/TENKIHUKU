226d4141b5207ec5ff044dc5e343381fc1318840
github yui-yoshida <yui.0618.kj8@gmail.com>
2018-12-03 20:12:24 +0900

    ローカル環境でもs3で保存されるように変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,15 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: 'ap-northeast-1'
+ }
+  config.fog_directory = ENV['S3_BUCKET_NAME']
+  config.asset_host = "https://s3-ap-northeast-1.amazonaws.com/#{ ENV['S3_BUCKET_NAME'] }"
+ end
 end
